1ece84223a9db8fb778f5b8f125de821b5dd2c0b
AlbyEgtn <mukradsumrad@gmail.com>
2024-09-14 12:11:54 +0700

    Latihan Modul 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,14 +6,16 @@
 </head>
 <body>
     <nav class="navbar navbar-dark bg-primary">
-        <div class="container"><a class="navbar-brand" href="/">Politeknik Negeri Bengkalis | D-IV Rakayasa Perangkat Lunak</a></div>
+        <div class="container"><a class="navbar-brand" href="/">Politeknik Negeri Bengkalis | D-IV Kemanan Sistem Informasi</a></div>
     </nav>
     <div class="container" style="margin-top: 150px">
         <div class="row">
             <div class="col-12 text-center">
                 <h1 class="text-secondary">Selamat Datang!</h1>
                 <h4 class="text-secondary">Di Perpustakaan Politeknik Negeri Bengkalis</h4>
-                <h6 class="mt-3"> Silahkan <a href="{{route('auth.login')}}" style="text-decoration: none"> masuk </a>atau <a href="{{route('auth.register')}}" style="text-decoration: none"> daftar </a> jika anda belum punya akun </h6>
+                <h6 class="mt-3"> Silahkan <a href="{{route('auth.login')}}" style="text-decoration: none"> masuk </a>
+                    atau
+                <a href="{{route('auth.register')}}" style="text-decoration: none"> daftar </a> jika anda belum punya akun </h6>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('auth.login');
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('auth.register');
+Route::get('/login', [LoginController::class, 'login'])->name('auth.login');
+Route::get('/register', [RegisterController::class, 'register'])->name('auth.register');
 
 Route::get('/', function () {
 return view('home');
